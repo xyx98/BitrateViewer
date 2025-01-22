6f480cf259e0e23e91802c601af347ca1d8ef53a
xyx98 <787633258@qq.com>
2025-01-22 10:31:20 +0800

    bump version to 0.5.0

--- config.h
+++ config.h
@@ -1,7 +1,7 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-#define BITRATEVIEWER_VERSION "0.4.0"
+#define BITRATEVIEWER_VERSION "0.5.0"
 
 #define ONLINE_ECHARTS "https://registry.npmmirror.com/echarts/5.6.0/files/dist/echarts.min.js"
 #define OFFLINE_ECHARTS "echarts.min.js"
